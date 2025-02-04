69bf3bfc68bfc11ad65d659bc3dcd44debcffe13
felipematheus1337 <lipehbr@gmail.com>
2025-02-04 13:08:56 -0300

    criado docker compose e métodos para collection.

--- src/main/java/com/vouchers/models/Voucher.java
+++ src/main/java/com/vouchers/models/Voucher.java
@@ -6,6 +6,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Document(collection = "vouchers")
 public class Voucher {
@@ -33,5 +34,123 @@ public class Voucher {
 
     private int usages;
 
+    public String getId() {
+        return id;
+    }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public boolean isUsable() {
+        return isUsable;
+    }
+
+    public void setUsable(boolean usable) {
+        isUsable = usable;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public LocalDateTime getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDateTime expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public VoucherType getType() {
+        return type;
+    }
+
+    public void setType(VoucherType type) {
+        this.type = type;
+    }
+
+    public VoucherStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(VoucherStatus status) {
+        this.status = status;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getUsages() {
+        return usages;
+    }
+
+    public void setUsages(int usages) {
+        this.usages = usages;
+    }
+
+    public Voucher() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Voucher voucher = (Voucher) o;
+        return Objects.equals(id, voucher.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Voucher{" +
+                "id='" + id + '\'' +
+                ", value=" + value +
+                ", balance=" + balance +
+                ", createdAt=" + createdAt +
+                ", isUsable=" + isUsable +
+                ", code='" + code + '\'' +
+                ", expirationDate=" + expirationDate +
+                ", type=" + type +
+                ", status=" + status +
+                ", description='" + description + '\'' +
+                ", usages=" + usages +
+                '}';
+    }
 }
